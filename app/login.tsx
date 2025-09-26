98e1f714127143b84dc6b1416b49a8e8e690a063
Manuel Duque Valencia <137368085+Praiseck@users.noreply.github.com>
2025-09-26 13:57:19 -0500

    Version 0.1 - Proyecto base con Expo, Typescript y Expo Router

---
+++ app/login.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function LoginScreen() {
+  return (
+    <View>
+      <Text>Pantalla de login</Text>
+    </View>
+  );
+}
---
+++ app/splash.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function SplashScreen() {
+  return (
+    <View>
+      <Text>Pantalla de carga</Text>
+    </View>
+  );
+}
