c62f1854b473836eb5bf8165368bda3bb33e4c6c
Manuel Duque Valencia <137368085+Praiseck@users.noreply.github.com>
2025-09-26 14:11:22 -0500

    Version 0.1 - Incorporacion pantalla de carga de prueba

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,9 +1,40 @@
-import { Text, View } from 'react-native';
+// app/splash.tsx
+import { router } from 'expo-router';
+import { useEffect } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function SplashScreen() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace('/login');
+    }, 2000); // espera 2 segundos antes de ir al login
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <View>
-      <Text>Pantalla de carga</Text>
+    <View style={styles.container}>
+      <Image source={require('../assets/images/splash-icon.png')} style={styles.logo} />
+      <Text style={styles.text}>MemoryExpress</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#1e1e1e',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+  },
+  text: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
